ef83fe8f3e6f509cd816698ad21c2b6620ea87ed
tsokovi <Ivaylo.Tsokov@endava.com>
2021-04-24 14:12:54 +0300

    .

--- PythonWebServer.ps1
+++ PythonWebServer.ps1
@@ -7,6 +7,7 @@ New-Item -Path $InstallPath -ItemType "directory"
 
 cd $InstallPath
 
+#Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #reload path
